ead12762b6eb2f12e0f7a3e09306ce2183fe26c5
G384602023 <238460@st.takushoku-u.ac.jp>
2024-06-20 16:27:09 +0900

    演習3-6

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,10 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+for(let n of campus.buildingD){
+	console.log(n);
+  }
+  for(let i = 0;i<gakka.length;i++){
+	console.log(gakka[i].name);
+  } 
