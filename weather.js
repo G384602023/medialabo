8bbbd2fb50ecd1ee09501360e963a42816c5cb93
G384602023 <238460@st.takushoku-u.ac.jp>
2024-06-22 15:11:56 +0900

    課題3-3

--- weather.js
+++ weather.js
@@ -46,5 +46,12 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
-console.log(data.coord.lon);
-
+console.log("緯度: "+data.coord.lon);
+console.log("経度: "+data.coord.lat);
+console.log("天気: "+data.weather[0].description);
+console.log("最低気温: "+data.main.temp_min);
+console.log("最高気温: "+data.main.temp_max); 
+console.log("湿度: "+data.main.humidity); 
+console.log("風速: "+data.wind.speed);
+console.log("風向: "+data.wind.deg);
+console.log("都市名: "+data.name);
